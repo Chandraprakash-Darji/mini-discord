5f1012674a7b4e949f84bc6ab4cd831067c2e086
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-13 22:43:42 +0530

    created ServerContext

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,32 +1,21 @@
-import { useEffect, useState } from 'react';
+import { createContext } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Nav } from '.';
-import { serverInfo } from '../data';
-import { getAllServerIds } from '../utils/getAllServerIds';
 import SideBar from './Sidebar';
 
+export const ServerContext = createContext('');
+
 function App() {
-    const [path, setPath] = useState<{ server: null | string; channel: null | string }>({
-        server: null,
-        channel: null,
-    });
-
-    useEffect(() => {
-        const path = window.location.pathname.split('/');
-        setPath({ server: path[1], channel: path[2] });
-        const allServer = getAllServerIds(serverInfo);
-        let isPresent = allServer.filter((server) => server === +path[1]);
-        if (isPresent.length === 0) setPath({ server: null, channel: null });
-    }, []);
-
-    console.log(path.server, path.channel);
+    const [_, server, channel] = useLocation().pathname.split('/');
 
     return (
-        <div className="min-h-screen bg-bgDark text-text flex">
-            <Nav />
-
-            <SideBar server={path.server} channel={path.channel} />
-            <div className="flex-1 bg-white/10"></div>
-        </div>
+        <ServerContext.Provider value={server}>
+            <div className="min-h-screen bg-bgDark text-text flex">
+                <Nav />
+                <SideBar server={server} channel={channel} />
+                <div className="flex-1 bg-white/10"></div>
+            </div>
+        </ServerContext.Provider>
     );
 }
 
