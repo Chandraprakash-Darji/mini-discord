54426aec26df6b0551832f9a20f2f9caa8712f68
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-14 01:06:54 +0530

    overfolow hidden for text name

--- src/components/Sidebar/RenderChannel.tsx
+++ src/components/Sidebar/RenderChannel.tsx
@@ -7,17 +7,19 @@ interface Props {
 
 const RenderChannel = ({ channels }: Props) => {
     return (
-        <div className="flex flex-col gap-1 text-grayText ">
+        <div className="flex flex-col text-grayText ">
             {channels.map((channel) => {
                 if (channel.type === 'text')
                     return (
                         <div
-                            className={`flex gap-2 w-full p-2 hover:bg-white/5 rounded-md items-center hover:text-text group `}
+                            className={`flex gap-2 w-full p-[5px] px-2 hover:bg-white/5 rounded-md items-center hover:text-text group `}
                             key={channel.id}
                         >
                             <span className="text-xl">#</span>
-                            <span className="mr-auto">{channel.label}</span>
-                            <div className="opacity-0 invisible group-hover:opacity-100 group-hover:visible flex">
+                            <span className="mr-auto overflow-hidden whitespace-nowrap text-ellipsis">
+                                {channel.label}
+                            </span>
+                            <div className="hidden group-hover:opacity-100 group-hover:flex">
                                 <PlusIcon />
                                 <PlusIcon />
                             </div>
@@ -26,14 +28,14 @@ const RenderChannel = ({ channels }: Props) => {
                 if (channel.type === 'voice')
                     return (
                         <div
-                            className={`flex gap-2 w-full p-2 hover:bg-white/5 rounded-md items-center hover:text-text group`}
+                            className={`flex gap-2 w-full p-[5px] px-2 hover:bg-white/5 rounded-md items-center hover:text-text group`}
                             key={channel.id}
                         >
                             <span className="text-xl">#</span>
                             <span className="mr-auto overflow-hidden whitespace-nowrap text-ellipsis">
                                 {channel.label}
                             </span>
-                            <div className="opacity-0 invisible group-hover:opacity-100 group-hover:visible flex">
+                            <div className="hidden group-hover:opacity-100 group-hover:flex">
                                 <PlusIcon />
                                 <PlusIcon />
                             </div>
@@ -43,10 +45,11 @@ const RenderChannel = ({ channels }: Props) => {
                     return (
                         <div
                             key={channel.id}
-                            className="flex flex-col w-full rounded-md justify-center"
+                            className="flex flex-col w-full rounded-md justify-center py-1"
                         >
-                            <span className="flex justify-between p-1 text-sm item-center">
-                                <span>{channel.label.toUpperCase()}</span>
+                            <span className="flex p-1 text-sm items-center overflow-hidden whitespace-nowrap text-ellipsis">
+                                <PlusIcon />
+                                <span className="mr-auto">{channel.label.toUpperCase()}</span>
                                 <PlusIcon />
                             </span>
                             <div className="pl-2">
