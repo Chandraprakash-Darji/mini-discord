8b8a1465155e29ce26ad56f154b50599c8593e4b
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-14 01:06:22 +0530

    Skeleton for channel

---
+++ src/components/Sidebar/Skelaton.tsx
@@ -0,0 +1,62 @@
+const Skelaton = () => {
+    return (
+        <>
+            <div className="py-5 px-3 flex flex-col gap-4">
+                <div className="w-20 h-5 bg-white/10 rounded-lg " />
+                <div className="flex gap-3 pl-3">
+                    <div className="w-5 rounded-full aspect-square bg-white/10" />
+                    <div className="w-20 h-5  rounded-lg bg-white/10" />
+                </div>
+                <div className="flex gap-3 pl-3">
+                    <div className="w-5 rounded-full aspect-square bg-white/10" />
+                    <div className="w-32 h-5  rounded-lg bg-white/10" />
+                </div>
+                <div className="flex gap-3 pl-3">
+                    <div className="w-5 rounded-full aspect-square bg-white/10" />
+                    <div className="w-16 h-5  rounded-lg bg-white/10" />
+                </div>
+                <div className="flex gap-3 pl-3">
+                    <div className="w-5 rounded-full aspect-square bg-white/10" />
+                    <div className="w-36 h-5  rounded-lg bg-white/10" />
+                </div>
+                <div className="flex gap-3 pl-3">
+                    <div className="w-5 rounded-full aspect-square bg-white/10" />
+                    <div className="w-28 h-5  rounded-lg bg-white/10" />
+                </div>
+                <div className="flex gap-3 pl-3">
+                    <div className="w-5 rounded-full aspect-square bg-white/10" />
+                    <div className="w-24 h-5  rounded-lg bg-white/10" />
+                </div>
+            </div>
+            <div className="py-5 px-3 flex flex-col gap-4">
+                <div className="w-20 h-5 bg-white/10 rounded-lg " />
+                <div className="flex gap-3 pl-3">
+                    <div className="w-5 rounded-full aspect-square bg-white/10" />
+                    <div className="w-32 h-5  rounded-lg bg-white/10" />
+                </div>
+                <div className="flex gap-3 pl-3">
+                    <div className="w-5 rounded-full aspect-square bg-white/10" />
+                    <div className="w-28 h-5  rounded-lg bg-white/10" />
+                </div>
+                <div className="flex gap-3 pl-3">
+                    <div className="w-5 rounded-full aspect-square bg-white/10" />
+                    <div className="w-12 h-5  rounded-lg bg-white/10" />
+                </div>
+                <div className="flex gap-3 pl-3">
+                    <div className="w-5 rounded-full aspect-square bg-white/10" />
+                    <div className="w-16 h-5  rounded-lg bg-white/10" />
+                </div>
+                <div className="flex gap-3 pl-3">
+                    <div className="w-5 rounded-full aspect-square bg-white/10" />
+                    <div className="w-24 h-5  rounded-lg bg-white/10" />
+                </div>
+                <div className="flex gap-3 pl-3">
+                    <div className="w-5 rounded-full aspect-square bg-white/10" />
+                    <div className="w-20 h-5  rounded-lg bg-white/10" />
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Skelaton;
