3817555640cd7bf013bc6beab44e5e3bedeeb980
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-14 04:19:07 +0530

    Container for Svg to givr custom Height

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from 'react';
 import { freeCodeCamp, ServerChannelsIn } from '../../data/serverinfo';
 import { useServerIds } from '../../utils/getAllServerIds';
 import CeveronDownicon from '../Icons/CeveronDownicon';
+import IconBtn from '../Icons/IconBtn';
 import RenderChannel from './RenderChannel';
 import Skelaton from './Skelaton';
+import UserProfile from './userProfile';
 
 interface Props {
     server: string;
@@ -45,18 +47,21 @@ const SideBar = ({ server, channel }: Props) => {
         );
     if (serverData)
         return (
-            <div id="sidebar" className="min-h-screen w-60 bg-[#ffffff11] font-bold text-sm">
+            <div id="sidebar" className="min-h-screen w-60 bg-[#ffffff11] font-bold text-sm ">
                 <div className="w-full">
                     <div className="h-12 flex justify-between items-center gap-3 p-3">
                         <span className="whitespace-nowrap overflow-hidden overflow-ellipsis ">
                             {serverData.label}
                         </span>
-                        <CeveronDownicon size={28} />
+                        <IconBtn size={28}>
+                            <CeveronDownicon />
+                        </IconBtn>
                     </div>
                     <div className="h-[.1rem] bg-bgDark" />
-                    <div className="channels py-3 p-1 h-[calc(100vh-48px)] overflow-y-scroll overflow-x-hidden relative">
+                    <div className="channels py-3 p-1 h-[calc(100vh-48px-52px)] overflow-y-scroll overflow-x-hidden relative">
                         <RenderChannel channels={serverData.channels} />
                     </div>
+                    <UserProfile />
                 </div>
             </div>
         );
