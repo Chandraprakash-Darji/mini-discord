83d5345f642d92cd3ff3b0303b5adba85d1d7ea7
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-14 03:22:05 +0530

    changed Icon and added unread state

--- src/components/Sidebar/RenderChannel.tsx
+++ src/components/Sidebar/RenderChannel.tsx
@@ -1,5 +1,8 @@
 import { channelIn, folderServerIn } from '../../data/serverinfo';
-import HashLockIcon from '../Icons/HashLOckIcon';
+import CeveronDownicon from '../Icons/CeveronDownicon';
+import GearIcon from '../Icons/GearIcon';
+import HashIcon from '../Icons/HashIcon';
+import PersonPlusIcon from '../Icons/PersonPlusIcon';
 import PlusIcon from '../Icons/plusIcon';
 import VoiceIcon from '../Icons/VoiceIcon';
 
@@ -9,41 +12,43 @@ interface Props {
 
 const RenderChannel = ({ channels }: Props) => {
     return (
-        <div className="flex flex-col text-grayText ">
+        <div className="flex flex-col text-grayText">
             {channels.map((channel) => {
                 if (channel.type === 'text')
                     return (
                         <div
-                            className={`flex gap-2 w-full p-[5px] px-2 hover:bg-white/5 rounded-md items-center hover:text-text group `}
+                            className={`flex gap-2 w-full p-[5px] px-2 hover:bg-white/5 rounded-md items-center hover:text-text/80  group after:w-1 after:bg-white after:absolute after:-left-0  after:rounded-r-full  ${
+                                channel.unread ? 'after:h-2 text-text' : 'after:h-0'
+                            } `}
                             key={channel.id}
                         >
-                            <span className="text-xl">
-                                <HashLockIcon />
+                            <span className="text-xl text-grayText">
+                                <HashIcon />
                             </span>
                             <span className="mr-auto overflow-hidden whitespace-nowrap text-ellipsis">
                                 {channel.label}
                             </span>
-                            <div className="hidden group-hover:opacity-100 group-hover:flex">
-                                <PlusIcon />
-                                <PlusIcon />
+                            <div className="hidden group-hover:opacity-100 group-hover:flex gap-1">
+                                <PersonPlusIcon />
+                                <GearIcon />
                             </div>
                         </div>
                     );
                 if (channel.type === 'voice')
                     return (
                         <div
-                            className={`flex gap-2 w-full p-[5px] px-2 hover:bg-white/5 rounded-md items-center hover:text-text group`}
+                            className={`flex gap-2 w-full p-[5px] px-2 hover:bg-white/5 rounded-md items-center hover:text-text/80 group`}
                             key={channel.id}
                         >
-                            <span className="text-xl">
+                            <span className="text-xl text-grayText">
                                 <VoiceIcon />
                             </span>
                             <span className="mr-auto overflow-hidden whitespace-nowrap text-ellipsis">
                                 {channel.label}
                             </span>
-                            <div className="hidden group-hover:opacity-100 group-hover:flex">
-                                <PlusIcon />
-                                <PlusIcon />
+                            <div className="hidden group-hover:opacity-100 group-hover:flex gap-1">
+                                <PersonPlusIcon />
+                                <GearIcon />
                             </div>
                         </div>
                     );
@@ -54,7 +59,7 @@ const RenderChannel = ({ channels }: Props) => {
                             className="flex flex-col w-full rounded-md justify-center py-1"
                         >
                             <span className="flex p-1 text-sm items-center overflow-hidden whitespace-nowrap text-ellipsis">
-                                <PlusIcon />
+                                <CeveronDownicon size={18} />
                                 <span className="mr-auto">{channel.label.toUpperCase()}</span>
                                 <PlusIcon />
                             </span>
