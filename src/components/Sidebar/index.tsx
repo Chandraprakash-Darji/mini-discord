8533157e32720d5fc4c463a7f9eade103bf487a5
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-13 23:58:59 +0530

    added channels to sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from 'react';
+import { freeCodeCamp, ServerChannelsIn } from '../../data/serverinfo';
+import { useServerIds } from '../../utils/getAllServerIds';
+import DiscordIcon from '../Icons/DiscordIcon';
+import RenderChannel from './RenderChannel';
+
 interface Props {
-    server: string | null;
-    channel: string | null;
+    server: string;
+    channel: string;
 }
 const SideBar = ({ server, channel }: Props) => {
-    if (!server && !channel)
+    const { allServerID } = useServerIds();
+    const [serverData, setServerData] = useState<ServerChannelsIn | null>(null);
+
+    let isPresent = allServerID.filter((ser) => ser === server).length > 0;
+    const [isLoading, setIsLoading] = useState(false);
+    useEffect(() => {
+        // if (!isLoading) setIsLoading(true);
+        // setTimeout(() => {
+        // if (freeCodeCamp.id === server) {
+        setServerData(freeCodeCamp);
+        // setIsLoading(false);
+        // }
+        // }, 0);
+    }, [server]);
+
+    if ((!server && !channel) || !isPresent)
         return (
-            <div id="sidebar" className="h-screen w-60 bg-[#ffffff11] overflow-auto">
+            <div id="sidebar" className="h-screen w-60 bg-[#ffffff11] overflow-hidden">
                 <div className="py-5 px-3 flex flex-col gap-4">
                     <div className="w-20 h-5 bg-white/10 rounded-lg " />
                     <div className="flex gap-3 pl-3">
@@ -62,12 +83,36 @@ const SideBar = ({ server, channel }: Props) => {
                 </div>
             </div>
         );
-    else
+    if (isLoading)
         return (
-            <div id="sidebar" className="h-screen w-60 bg-[#ffffff11]">
-                Op
+            <div className="h-screen w-60 bg-[#ffffff11] aspect-square flex justify-center items-center">
+                <div className="relative">
+                    <div className="w-10 absolute aspect-square border-4 border-bgDark rounded-full left-0 top-0 " />
+                    <div className="animate-spin w-10 aspect-square border-4 border-transparent border-l-white rounded-full" />
+                </div>
             </div>
         );
+    if (serverData)
+        return (
+            <div
+                id="sidebar"
+                className="h-screen w-60 bg-[#ffffff11] font-bold text-sm overflow-y-auto overflow-x-hidden"
+            >
+                <div className="w-full">
+                    <div className="h-14 flex justify-between items-center gap-3 p-3">
+                        <span className="whitespace-nowrap overflow-hidden overflow-ellipsis ">
+                            {serverData.label}
+                        </span>
+                        <DiscordIcon />
+                    </div>
+                    <div className="h-1 bg-bgDark" />
+                    <div className="pt-3 p-1">
+                        <RenderChannel channels={serverData.channels} />
+                    </div>
+                </div>
+            </div>
+        );
+    return <>Something Went Wrong</>;
 };
 
 export default SideBar;
