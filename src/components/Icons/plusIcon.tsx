fb51e3c4c9e5133bc3f3d4c9f7a3fd559a165203
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-14 03:20:55 +0530

    Icon size able to change now bby size prop

--- src/components/Icons/plusIcon.tsx
+++ src/components/Icons/plusIcon.tsx
@@ -1,18 +1,17 @@
-const PlusIcon = () => {
+import { SvgProps } from '../../data/serverinfo';
+
+const PlusIcon = ({ size }: SvgProps) => {
+    const sizeIcon = size ? size : 18;
     return (
-        <svg
-            aria-hidden="true"
-            role="img"
-            width="18"
-            height="18"
-            viewBox="0 0 18 18"
-        >
-            <polygon
-                fill-rule="nonzero"
-                fill="currentColor"
-                points="15 10 10 10 10 15 8 15 8 10 3 10 3 8 8 8 8 3 10 3 10 8 15 8"
-            ></polygon>
-        </svg>
+        <span style={{ width: sizeIcon, aspectRatio: '1 / 1' }}>
+            <svg aria-hidden="true" role="img" width="100%" height="100%" viewBox="0 0 18 18">
+                <polygon
+                    fill-rule="nonzero"
+                    fill="currentColor"
+                    points="15 10 10 10 10 15 8 15 8 10 3 10 3 8 8 8 8 3 10 3 10 8 15 8"
+                ></polygon>
+            </svg>
+        </span>
     );
 };
 
