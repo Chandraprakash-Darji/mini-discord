040b3f8e83c06b1c912979fddefab813a6e790fd
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-14 12:53:31 +0530

    added navigate to /@me when acess of /

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
-import { createContext } from 'react';
-import { useLocation } from 'react-router-dom';
+import { createContext, useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Nav } from '.';
 import SideBar from './Sidebar';
 
@@ -7,13 +7,22 @@ export const ServerContext = createContext('');
 
 function App() {
     const [_, server, channel] = useLocation().pathname.split('/');
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!server && !channel) {
+            console.log(server, channel);
+            navigate('/@me');
+        }
+    }, []);
 
     return (
         <ServerContext.Provider value={server}>
             <div className="h-screen bg-bgDark text-text flex overflow-y-hidden">
                 <Nav />
                 <SideBar server={server} channel={channel} />
-                <div className="flex-1 bg-white/10"></div>
+                <div className="flex-1 bg-white/10">
+                    
+                </div>
             </div>
         </ServerContext.Provider>
     );
