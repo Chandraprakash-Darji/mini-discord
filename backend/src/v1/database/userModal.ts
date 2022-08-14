6af494496a2991938ee8070e169ba58f3e424e1d
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-14 23:52:36 +0530

    added: user Modal

---
+++ backend/src/v1/database/userModal.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, "Please provide an Email!"],
+        unique: [true, "Email Exist"],
+    },
+
+    password: {
+        type: String,
+        required: [true, "Please provide a password!"],
+        unique: false,
+    },
+});
+
+export const User = mongoose.model("User", userSchema);
