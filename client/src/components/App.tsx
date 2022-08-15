a38c13e1f01b8d9c4c17c47a4fb77d144ef669a1
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-15 17:48:37 +0530

    Setup for Login And register Routes

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,29 +1,34 @@
 import { createContext, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import { Nav } from '.';
 import SideBar from './Sidebar';
+import Cookies from 'universal-cookie';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 export const ServerContext = createContext('');
+export const cookies = new Cookies();
 
 function App() {
     const [_, server, channel] = useLocation().pathname.split('/');
     const navigate = useNavigate();
     useEffect(() => {
+        const token = cookies.get('TOKEN');
+        if (!token) navigate('/login');
         if (!server && !channel) {
-            console.log(server, channel);
-            navigate('/@me');
+            navigate('/channels/5');
         }
-        
     }, []);
 
     return (
         <ServerContext.Provider value={server}>
             <div className="h-screen bg-bgDark text-text flex overflow-y-hidden">
-                <Nav />
-                <SideBar server={server} channel={channel} />
-                <div className="flex-1 bg-white/10">
-                    
-                </div>
+                <Routes>
+                    <Route path="/channels/:id/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
             </div>
         </ServerContext.Provider>
     );
