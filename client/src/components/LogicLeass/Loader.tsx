a24e78127e27104029fedb4fb0969b18fa8b25cd
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-15 17:45:29 +0530

    Removed margin from Loader

--- client/src/components/LogicLeass/Loader.tsx
+++ client/src/components/LogicLeass/Loader.tsx
@@ -1,8 +1,8 @@
 const Loader = () => {
     return (
         <div className="w-10 h-10 aspect-square">
-            <div className=" absolute w-10 h-10 border-4 border-bgDark rounded-full top-16" />
-            <div className="animate-spin w-full h-full border-4 border-transparent border-l-white rounded-full mt-16" />
+            <div className=" absolute w-10 h-10 border-4 border-bgDark rounded-full " />
+            <div className="animate-spin w-full h-full border-4 border-transparent border-l-white rounded-full" />
         </div>
     );
 };
