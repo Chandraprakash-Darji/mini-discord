73175c1643ec7e2443adf8f5cf0c5667c0bfbedb
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-16 20:56:15 +0530

    Removed api Routes

--- backend/src/v1/routes/authRoute.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { loginUser, registerUser } from "../controllers/authController";
-
-const authRouter = express.Router();
-
-authRouter.post("/register", registerUser);
-authRouter.post("/login",loginUser)
-
-export default authRouter;
